e3ada0ae052bcdbb2e7f684c4d72085b1453efbf
Alexandra <alexandrafernandezquispe@gmail.com>
2016-09-07 17:27:34 -0500

    ejercicio4

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+  var boton = document.getElementById("iniciar");
+    boton.addEventListener("click", function() {
+        var segundos = parseInt(document.getElementById("segundos").value);
+        setInterval(color, segundos*1000)
+            function color(){ 
+                document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        }
+    });
+});
